19b2dd0084d9c65cda994a6c6cca1ebfee6ef812
Muhammed Kasem <muhammedkasem2002@gmail.com>
2022-07-27 16:03:14 -0400

    Updated the main game function and reworked the logic that determins the game winner.

--- script.js
+++ script.js
@@ -1,56 +1,64 @@
+// Array of choices that the computer can pick from.
 const choice = ['Rock', 'Paper', 'Scissors']
 
+// This function grabs a random value from the above array and assigns it as the computers choice.
 function getComputerChoice() {
   let compChoice = choice[Math.floor(Math.random() * choice.length)];
   console.log(compChoice);
-  return compChoice; // This function will give me one of the 3 options I defined in the choice array back.
+  return compChoice;
 }
 
-
+// This function prompts the user for an input and stores their selection to be matched up later with the computers choice.
 function getPlayerSelection() {
   let playerSelection = prompt("Enter rock paper or scissors:")
   return playerSelection;
 }
 
+// This function takes the user and computer selection that will be grabbed with the two functions above and returns
+// a value that determines who won the round.
 function playRound(playerSelection, computerSelection) {
 
   if (playerSelection.toLowerCase() == computerSelection.toLowerCase()) {
-    return alert("This round is a draw!"), "Its a draw!";
+    return "draw";
   }
   else if (playerSelection.toLowerCase() == 'rock') {
-    return (computerSelection.toLowerCase() == 'paper') ? "You lose": "You win";
+    return (computerSelection.toLowerCase() == 'paper') ? "lose" : "win";
   }
   else if (playerSelection.toLowerCase() == 'paper') {
-    return (computerSelection.toLowerCase() == 'scissors') ? "You lose": "You win";
+    return (computerSelection.toLowerCase() == 'scissors') ? "lose" : "win";
   }
   else if (playerSelection.toLowerCase() == 'scissors') {
-    return (computerSelection.toLowerCase() == 'rock') ? "You lose": "You win";
+    return (computerSelection.toLowerCase() == 'rock') ? "lose" : "win";
   }
 }
-
-function game () {
+// This function is the only function that is called to run the program and plays a total of 5 rounds. The for loop
+// breaks once a user reaches 5 wins and alerts the outcome of the game.
+function game() {
   let playerRoundWins = 0;
   let computerRoundWins = 0;
   for (let i = 0; i < 5; i++) {
-    if (playRound(getPlayerSelection(),getComputerChoice()) == "You win") {
-      playerRoundWins +=1;
-      alert("You won this round!");
+    let outcome = playRound(getPlayerSelection(), getComputerChoice());
+
+    switch (outcome) {
+      case 'draw':
+        alert("This round is a draw!");
+        i -= 1;
+        break;
+
+      case 'win':
+        alert("You won this round!");
+        playerRoundWins += 1;
+        break;
+
+      case 'lose':
+        alert("You lost this round!");
+        computerRoundWins += 1;
+        break;
     }
-    else if (playRound(getPlayerSelection(),getComputerChoice()) == "You lose") {
-      computerRoundWins +=1;
-      alert("You lose this round!");
-    }
-    else if (playRound(getPlayerSelection(),getComputerChoice()) == "Its a draw!") {
-      alert("This round is a draw!");
-    }
-    // (playRound(getPlayerSelection(),getComputerChoice()) == "You win") ? playerRoundWins += 1 : computerRoundWins += 1;
   }
   (playerRoundWins > computerRoundWins) ? alert("Congrats you won the best of 5 rounds!") : alert("Sorry you lose! Better luck next time!");
-  }
-
-
+}
 
 
 game();
-// console.log(playRound(getPlayerSelection(), getComputerChoice()));
 
